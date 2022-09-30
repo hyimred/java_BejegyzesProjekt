cc4348567143c0401823dbd4337feb1f20db4627
hyimred <ptrk.csak@gmail.com>
2022-09-30 15:10:15 +0200

    like tostring metodusok (Bejegyzes osztaly)

--- src/Bejegyzes.java
+++ src/Bejegyzes.java
@@ -1,45 +1,57 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Bejegyzes {
     private String szerzo;
     private String tartalom;
-    private int likeok;
-    private LocalDateTime letrejott;
+    private int likeok = 0;
+    private LocalDateTime letrejott = LocalDateTime.now();
     private LocalDateTime szerkesztve;
-
-    public Bejegyzes(String szerzo, String tartalom, int likeok, LocalDateTime letrejott, LocalDateTime szerkesztette) {
+    public Bejegyzes(String szerzo, String tartalom) {
         this.szerzo = szerzo;
         this.tartalom = tartalom;
         this.likeok = likeok;
         this.letrejott = letrejott;
         this.szerkesztve = szerkesztve;
     }
-
     public String getSzerzo() {
         return szerzo;
     }
-
     public String getTartalom() {
         return tartalom;
     }
-
     public int getlikeok() {
         return likeok;
     }
-
     public LocalDateTime getLetrejott() {
         return letrejott;
     }
-
     public LocalDateTime getszerkesztve() {
         return szerkesztve;
     }
-
     public void setTartalom(String tartalom) {
         this.tartalom = tartalom;
     }
+    public void like(int i) {
+        for (int j = 0; j < i; j++) {
+            this.likeok++;
+        }
+    }
+    public void szerkesztett(String ujTartalom) {
+        if (this.tartalom != ujTartalom) {
+            this.szerkesztve = LocalDateTime.now();
+            this.tartalom = ujTartalom;
+            boolean szerkesztett = true;
+        }
+    }
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String bejegyzesAdat = String.format("%s - %d - %s",
+                this.szerzo, this.likeok, this.letrejott.format(formatter));
 
-    public void like() {
+        bejegyzesAdat += String.format("\n%s",this.tartalom);
 
+        return bejegyzesAdat;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bejegyzes teszt = new Bejegyzes("JohnDoe", "Ez egy teszt...\n");
+        System.out.println(teszt);
+        teszt.like(30);
+        System.out.println(teszt);
+
     }
 }
